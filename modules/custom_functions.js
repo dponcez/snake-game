bc672e1abcf93825c3de9bada456d2161bf13dc5
Damian Ponce <dponcez_git@hotmail.com>
2025-02-09 01:36:28 -0500

    feat: add new custom functions (log) and (selectorAll)

--- modules/custom_functions.js
+++ modules/custom_functions.js
@@ -1,5 +1,7 @@
 export const selector = (element) => document.querySelector(element);
+export const selectorAll = (element) => document.querySelectorAll(element);
 export const createElement = (element) => document.createElement(element);
+export const log = (value) => console.log(value);
 export const generateGameElement = (size) => {
   const x = Math.floor(Math.random() * size) + 1;
   const y = Math.floor(Math.random() * size) + 1;
